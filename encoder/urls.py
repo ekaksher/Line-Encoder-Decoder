9444c0d41c0bd6d6b77bf2d45404bfe2c9ddb85d
ekaksher <ekaksher@live.in>
2020-11-22 22:49:21 +0530

    Creating Views

--- encoder/urls.py
+++ encoder/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('encode/',views.encode,name='encode'),
+    path('decode/',views.decode,name='decode'),
 ]
--- encoder/views.py
+++ encoder/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return render(request,'encoder/home.html')
+def encode(request):
+    return render(request,'encoder/encode.html')
+def decode(request):
+    return render(request,'encoder/decode.html')
